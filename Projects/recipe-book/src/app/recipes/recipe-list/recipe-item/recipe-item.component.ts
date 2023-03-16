7343b2e89ee449f4efd71ce54cb0585950c0d08a
Valwin <87019339+valz1278@users.noreply.github.com>
2023-03-16 16:47:39 +0800

    Passing recipe data with property binding

--- Projects/recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ Projects/recipe-book/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+
+import { Recipe } from './../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent {
+  @Input() recipe: Recipe;
+  @Output() selectedRecipe = new EventEmitter<void>();
 
+  onSelected() {
+    this.selectedRecipe.emit();
+  }
 }

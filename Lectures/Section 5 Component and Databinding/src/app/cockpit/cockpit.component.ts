9455ceaa2cd64615016ac6cefbd6cbf6758eec45
Valwin <87019339+valz1278@users.noreply.github.com>
2023-03-16 00:43:54 -0700

    Section 5: Components and Databinding (#6)
    * Initial project
    
    * Splitting Apps into Components
    
    * Binding to custom properties
    
    * Binding to custom events
    
    * Local references, @ViewChild and ng-content
    
    * Lifecycle hooks template access and ng-content access
    
    * Rename folder

---
+++ Lectures/Section 5 Component and Databinding/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
+}

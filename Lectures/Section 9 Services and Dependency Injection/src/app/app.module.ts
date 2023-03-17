aafec2f4b3c88198ce12a2e5f3262b35617d6baf
Valwin <87019339+valz1278@users.noreply.github.com>
2023-03-17 14:58:25 +0800

    Injecting Services into Services

--- Lectures/Section 9 Services and Dependency Injection/src/app/app.module.ts
+++ Lectures/Section 9 Services and Dependency Injection/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
+import { AccountsService } from './accounts.service';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { NewAccountComponent } from './new-account/new-account.component';
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [AccountsService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

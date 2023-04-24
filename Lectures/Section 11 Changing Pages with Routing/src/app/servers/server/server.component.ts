017c585d158799967214c2355540b741e4e7b417
Valwin <87019339+valz1278@users.noreply.github.com>
2023-04-24 16:24:58 +0800

    Using query parameters

--- Lectures/Section 11 Changing Pages with Routing/src/app/servers/server/server.component.ts
+++ Lectures/Section 11 Changing Pages with Routing/src/app/servers/server/server.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -6,24 +6,26 @@ import { ServersService } from '../servers.service';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
   constructor(
     private serversService: ServersService,
     private route: ActivatedRoute,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-      }
-    )
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+  }
 }

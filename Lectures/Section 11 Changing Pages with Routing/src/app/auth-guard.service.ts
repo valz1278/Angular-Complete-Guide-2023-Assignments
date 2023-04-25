63e06ccca5c21600e28339beb0ff4523e34cddcb
Valwin <87019339+valz1278@users.noreply.github.com>
2023-04-25 17:27:17 +0800

    protecting routes with canActivate

---
+++ Lectures/Section 11 Changing Pages with Routing/src/app/auth-guard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) return true;
+      else {
+        this.router.navigate(['/']);
+      }
+    });
+    return false;
+  }
+}
